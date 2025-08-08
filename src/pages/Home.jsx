34d7b368a760fba9cf03c4ce9cf05c22b31f01b9
adyaananyamishra <adyaananyamishra@email.com>
2025-08-08 13:12:40 +0530

    chore: added pricing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import AboutSection from '../components/AboutSection';
 import ChooseUs from '../components/ChooseUs';
 import Superiority from '../components/Superiority';
 import IndustryExpertise from '../components/IndustryExpertise';
+import ShippingCalculator from '../components/ShippingCalculator';
 import Testimonial from '../components/Testimonial';
 import TrustedPartners from '../components/TrustedPartners';
 
@@ -19,6 +20,7 @@ const Home = () => {
       <ChooseUs/>
       <Superiority/>
       <IndustryExpertise/>
+      <ShippingCalculator/>
       <Testimonial/>
       <TrustedPartners/>
     </>
